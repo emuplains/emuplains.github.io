f4063ab7169ba39a3b3622508e13d94614cbb0a3
Jiawei Zhang <jiawei.zhang@team.telstra.com>
2017-06-06 14:34:58 +1000

    Added test entry for uninstalled app.

--- redirect/resources/js/apps.js
+++ redirect/resources/js/apps.js
@@ -12,18 +12,32 @@ if (extraParameters.length>0) {
 	extraParameters = '';
 }
 window.sypportedApps = {
-	'24x7':{
-        iosApp: 't24x7://launch?'+extraParameters,
-        iosAppStore: 'https://itunes.apple.com/au/app/telstra-24x7-for-iphone/id543829966?mt=8',
-        // For this, your app need to have category filter: android.intent.category.BROWSABLE
-        android: {
-            'host': 'launch?' + extraParameters, // Host/path/querystring part in a custom scheme URL
-            // 'action': '', // Equivalent to ACTION in INTENTs
-            // 'category': '', // Equivalent to CATEGORY in INTENTs
-            // 'component': '', // Equivalent to COMPONENT in INTENTs
-            'scheme': 't24x7', // Scheme part in a custom scheme URL
-            'package': 'com.telstra.mobile.android.mytelstra', // Package name in Play store
-            'fallback': 'https://play.google.com/store/apps/details?id=com.telstra.mobile.android.mytelstra&hl=en'
-        }
-	}	
+	'24x7': {
+		iosApp: 't24x7://launch?' + extraParameters,
+		iosAppStore: 'https://itunes.apple.com/au/app/telstra-24x7-for-iphone/id543829966?mt=8',
+		// For this, your app need to have category filter: android.intent.category.BROWSABLE
+		android: {
+			'host': 'launch?' + extraParameters, // Host/path/querystring part in a custom scheme URL
+			// 'action': '', // Equivalent to ACTION in INTENTs
+			// 'category': '', // Equivalent to CATEGORY in INTENTs
+			// 'component': '', // Equivalent to COMPONENT in INTENTs
+			'scheme': 't24x7', // Scheme part in a custom scheme URL
+			'package': 'com.telstra.mobile.android.mytelstra', // Package name in Play store
+			'fallback': 'https://play.google.com/store/apps/details?id=com.telstra.mobile.android.mytelstra&hl=en'
+		}
+	},
+	'na': {
+		iosApp: 'none://launch?' + extraParameters,
+		iosAppStore: 'https://itunes.apple.com/au/app/telstra-24x7-for-iphone/id543829966?mt=8',
+		// For this, your app need to have category filter: android.intent.category.BROWSABLE
+		android: {
+			'host': 'launch?' + extraParameters, // Host/path/querystring part in a custom scheme URL
+			// 'action': '', // Equivalent to ACTION in INTENTs
+			// 'category': '', // Equivalent to CATEGORY in INTENTs
+			// 'component': '', // Equivalent to COMPONENT in INTENTs
+			'scheme': 't24x7', // Scheme part in a custom scheme URL
+			'package': 'com.none.app', // Package name in Play store
+			'fallback': 'https://play.google.com/store/apps/details?id=com.telstra.mobile.android.mytelstra&hl=en'
+		}
+	}
 };
